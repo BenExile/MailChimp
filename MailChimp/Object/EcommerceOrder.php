9c82b0b932d9d310e9eb0b54540c1eeb612de8e9
Ben Tadiar <ben@handcraftedbyben.co.uk>
2012-08-14 19:05:59 +0100

    EcommerceOrder now extends EcommerceOrderAbstract

--- MailChimp/Object/EcommerceOrder.php
+++ MailChimp/Object/EcommerceOrder.php
@@ -1,76 +1,17 @@
 <?php
 
 /**
- * A convenience object that prepares the parameters
- * that are required by the campaignEcommOrderAdd() API call
+ * Ecommerce order object
+ * Prepares the parameters that are required by
+ * the campaignEcommOrderAdd() API call
  * @author Ben Tadiar <ben@handcraftedbyben.co.uk>
  * @package MailChimp
  * @subpackage Object
  */
 namespace MailChimp\Object;
 
-class EcommerceOrder implements EcommerceOrderInterface
+class EcommerceOrder extends EcommerceOrderAbstract
 {
-    /**
-     * The store's internal ID for the order
-     * @var null|string
-     */
-    protected $orderID = null;
-    
-    /**
-     * The mc_cid recorded from an email click
-     * @var null|string
-     */
-    protected $campaignID = null;
-    
-    /**
-     * The mc_eid recorded from an email click
-     * @var null|string
-     */
-    protected $emailID = null;
-    
-    /**
-     * The unique, user-defined ID for the store sending the order in
-     * @var null|string
-     */
-    protected $storeID = null;
-    
-    /**
-     * Optional "nice" store name for associated store ID
-     * @var null|string
-     */
-    protected $storeName = null;
-    
-    /**
-     * Date for the order
-     * @var string
-     */
-    protected $date = null;
-    
-    /**
-     * Array of items for the order
-     * @var array
-     */
-    protected $items = array();
-    
-    /**
-     * Total cost for all items
-     * @var double
-     */
-    protected $itemsCost = 0;
-    
-    /**
-     * Cost of shipping this order
-     * @var double
-     */
-    protected $shipping = null;
-    
-    /**
-     * Tax paid on this order
-     * @var double
-     */
-    protected $tax = null;
-    
     /**
      * Set the order ID, email of subscriber to attach the order to and order date
      * @param string $orderID The store's internal ID for the order
--- MailChimp/Object/EcommerceOrderInterface.php
+++ MailChimp/Object/EcommerceOrderInterface.php
@@ -2,6 +2,7 @@
 
 /**
  * MailChimp Ecommerce Order Interface
+ * @todo Specify methods
  * @author Ben Tadiar <ben@handcraftedbyben.co.uk>
  * @package MailChimp
  * @subpackage Object
